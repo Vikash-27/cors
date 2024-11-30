85a565167a5216d109446374e8b1aabeba984c39
Vikash-27 <kandukuribalavikash.20.csd@anits.edu.in>
2024-12-01 00:10:11 +0530

    fixeed

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-// Import required modules
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const cors = require('cors');
 
-// Create an Express app
 const app = express();
 const PORT = 3001;
 
-// Middleware to parse JSON requests
 app.use(express.json());
-
-// Enable CORS
 app.use(cors());
 
-// Define CORS middleware
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -21,7 +15,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Define Swiggy API proxy route
 app.use('/api/proxy/swiggy/dapi', createProxyMiddleware({
   target: 'https://www.swiggy.com',
   changeOrigin: true,
@@ -30,12 +23,10 @@ app.use('/api/proxy/swiggy/dapi', createProxyMiddleware({
   },
 }));
 
-// Test route
 app.get('/', (req, res) => {
   res.send('<h1>Welcome to the Food Delivery App</h1>');
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
